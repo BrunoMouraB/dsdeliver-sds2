9f305a5dfc1e1b425949c21b48b9a0b5b9ef73dc
BrunoMouraB <brunnomhoura@gmail.com>
2021-01-09 01:24:51 -0300

    Search order location

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,15 +1,17 @@
-import './styles.css';
+import './styles.css'
 import {ReactComponent as Logo} from './logo.svg';
 import { Link } from 'react-router-dom';
 
-function Navbar () {
+function Navbar() {
     return (
-        <nav className="main-navbar">
-            <Logo />
-            <Link to="/" className="logo-text"> DS Delivery</Link>
+       <nav className="main-navbar">
+        <Logo />
+        <Link to="/"  className="logo-text">
+            DS Delivery</Link>
         
-        </nav>
+       </nav>
     )
 }
 
 export default Navbar;
+
